c65b4615c91651ddc4c0ea7403e7f225ab5bbb8f
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-05-25 09:16:12 +0900

    Hilt_Retrofit_Coroutine Branch

--- app/src/main/java/com/jomi/weitstudy/MainActivity.kt
+++ app/src/main/java/com/jomi/weitstudy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jomi.weitstudy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Hilt_Retrofit_Coroutine Branch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
