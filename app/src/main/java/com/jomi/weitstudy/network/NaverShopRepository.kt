2764b98c926730c8748f1b1ec5c0eba7262437b4
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-03 14:51:56 +0900

    chore : 사용하지 않는 코드 제거

--- app/src/main/java/com/jomi/weitstudy/network/NaverShopRepository.kt
+++ app/src/main/java/com/jomi/weitstudy/network/NaverShopRepository.kt
@@ -6,24 +6,7 @@ class NaverShopRepository @Inject constructor(private val naverShopService : Nav
 
     suspend fun naverShopSearch(query: String, display: Int, start: Int) =
         naverShopService.getSearchShop(query, display, start)
-
-
-//    fun makeApiCall(query:String, display: Int, start: Int, liveDataList: MutableLiveData<List<NaverShopItem>>){
-//        val call: Call<NaverShopResponse> = naverShopService.getSearchShop(query, display, start)
-//        call?.enqueue(object : Callback<NaverShopResponse>{
-//            override fun onResponse(
-//                call: Call<NaverShopResponse>,
-//                response: Response<NaverShopResponse>
-//            ) {
-//                liveDataList.postValue(response.body()?.items!! as List<NaverShopItem>?)
-//            }
-//
-//            override fun onFailure(call: Call<NaverShopResponse>, t: Throwable) {
-//                Log.d("Tag", t.message.toString())
-//                liveDataList.postValue(null)
-//            }
-//        })
-//    }
+    
 }
 
 
