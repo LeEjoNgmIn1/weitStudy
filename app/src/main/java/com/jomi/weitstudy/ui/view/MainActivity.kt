460414d4411eb0272076bb44be419b8da5d18a94
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-13 10:31:15 +0900

    feat : MainActivity 추가
    검색기능이 있는 메인 activity xml 추가

---
+++ app/src/main/java/com/jomi/weitstudy/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jomi.weitstudy.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jomi.weitstudy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
