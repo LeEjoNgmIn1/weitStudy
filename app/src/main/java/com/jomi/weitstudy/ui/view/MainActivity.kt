4604b477b31f4ea1727f606c1f988434630bb99e
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-13 10:52:04 +0900

    feat : mainActivity의 textfield의 텍스트가 변경 되었을때 기능 추가
    1. 글자가 입력되면 helperText 삭제
    2. 글자가 입력되면 스크롤뷰를 올리기
    3. 입력된글자가 12글자가 되면 토스트 알림 띄우기

--- app/src/main/java/com/jomi/weitstudy/ui/view/MainActivity.kt
+++ app/src/main/java/com/jomi/weitstudy/ui/view/MainActivity.kt
@@ -2,8 +2,10 @@ package com.jomi.weitstudy.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.jomi.weitstudy.R
 import com.jomi.weitstudy.databinding.ActivityMainBinding
+import com.jomi.weitstudy.others.onMyTextChanged
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 텍스트가 변경되었을떄
+        binding.etMainSearch.onMyTextChanged {
+            // 글자가 입력 되면
+            if(it.toString().isNotEmpty()){
+                // helperText 삭제
+                binding.tfMainSearch.helperText = ""
+                // 스크롤뷰를 올리기
+                binding.svMain.scrollTo(0, 200)
+            }
+            // 입력된 글자가 12글자 이상일떄 토스트 띄우기
+            if(it.toString().count() == 12){
+                Toast.makeText(this, "검색어는 12자까지만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
