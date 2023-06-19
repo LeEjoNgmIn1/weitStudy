5baf3280d23dcfc1f0a2240b18fa0261098e1920
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-19 14:50:18 +0900

    no message

---
+++ app/src/main/java/com/jomi/weitstudy/data/model/LikeItems.kt
@@ -0,0 +1,15 @@
+package com.jomi.weitstudy.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "like_item_table")
+data class LikeItems(
+    @PrimaryKey(autoGenerate = false)
+    var productId: String,
+    val lprice: String,
+    val mallName: String,
+    val title: String,
+
+    var numLike : Int = 0
+)
